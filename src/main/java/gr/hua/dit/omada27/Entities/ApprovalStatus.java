35808d0266378c38e73b61ba6c888dabd8594a87
IONNYT <156337839+IONNYT@users.noreply.github.com>
2025-01-17 12:41:41 +0200

    Delete src directory

--- src/main/java/gr/hua/dit/omada27/Entities/ApprovalStatus.java
+++
@@ -1,9 +0,0 @@
-package gr.hua.dit.omada27.Entities;
-
-// enum κλάση, χρησιμοποιείται για το approval status των καταχωρίσεων ακινήτων (registration)
-// και για το approval status αιτημάτων ενικοιαστών (request)
-public enum ApprovalStatus {
-    PENDING,
-    ACCEPTED,
-    REJECTED
-}
--- src/main/java/gr/hua/dit/omada27/Entities/VerificationStatus.java
+++
@@ -1,7 +0,0 @@
-package gr.hua.dit.omada27.Entities;
-
-// enum κλάση για το verification των ενικοιαστώνΣ
-public enum VerificationStatus {
-    UNVERIFIED,
-    VERIFIED
-}
